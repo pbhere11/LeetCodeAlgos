a59c4a3f2d694b119f47d646d21c558d211987c1
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-05-16 15:27:47 -0400

    Valid Anagram

--- Easy/Strings/ValidAnagram.java
+++ Easy/Strings/ValidAnagram.java
@@ -6,6 +6,34 @@ Output: false
 */
 class Solution {
     public boolean isAnagram(String s, String t) {
-        
+        int[] countArr = new int[26];
+        String str1 = "";
+        String str2 = "";
+        if(s.length()<=t.length())
+        {
+        	str1 = s.toLowerCase();
+        	str2 = t.toLowerCase();
+        }
+        else
+        {
+        	str2 = s.toLowerCase();
+        	str1 = t.toLowerCase();
+        }
+        for(int i=0;i<str1.length();i++)
+        {
+        	countArr[str1.charAt(i)-97]++;
+        }
+        for(int i=0;i<str2.length();i++)
+        {
+        	if(countArr[str2.charAt(i)-97]<1)
+        	{
+        		return false;
+        	}
+        	else
+        	{
+        		countArr[str2.charAt(i)-97]--;
+        	}
+        }
+        return true;
     }
 }
