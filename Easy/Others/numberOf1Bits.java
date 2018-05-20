b9fd391600a553cbcf5b82c83182b2558b679a6f
Pritesh.Bhere <pbhere11@gmail.com>
2018-05-20 16:16:11 -0400

    Hamming distance

---
+++ Easy/Others/numberOf1Bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0)
+        {
+        	if(n%2==1)
+        	{
+        		count++;
+        	}
+        	n = n/2;
+        }
+        return count;
+    }
+}
