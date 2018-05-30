4ab57ae3ae828f0c5188f7297dc28e3e6a3834b5
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-05-30 14:07:03 -0400

    adding the word search

--- Moderate/Backtracking/WordSearch.java
+++ Moderate/Backtracking/WordSearch.java
@@ -9,10 +9,31 @@ board =
 Given word = "ABCCED", return true.
 Given word = "SEE", return true.
 Given word = "ABCB", return false.
+
+[
+["C","A","A"],
+["A","A","A"],
+["B","C","D"]
+]
+AAB
+
+[
+["A","B","C","E"],
+["S","F","E","S"],
+["A","D","E","E"]
+]
+ABCESEEEFS
 */
 class Solution {
     public boolean exist(char[][] board, String word) {
-    	char[][] ogboard = board;
+    	char[][] ogboard = new char[board.length][board[0].length];
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[i].length;j++)
+            {
+                ogboard[i][j]=board[i][j];
+            }
+        }
         for(int i=0;i<board.length;i++)
         {
         	for(int j=0;j<board[i].length;j++)
@@ -21,7 +42,13 @@ class Solution {
 				{
 					return true;
 				}
-				board = ogboard;
+				for(int x=0;x<board.length;x++)
+                {
+                    for(int y=0;y<board[i].length;y++)
+                    {
+                        board[x][y]=ogboard[x][y];
+                    }
+                }
         	}
         }
         return false;
@@ -29,13 +56,15 @@ class Solution {
 
     private boolean exist(char[][] board, String word, int wordIndex, int i, int j)
     {
-    	if(i>=0 && i<board.length && j>=0 && j<board[i].length && board[i][j]!= '0' && wordIndex<word.length() && word.charAt(wordIndex)==board[i][j])
+    	if(i>=0 && i<board.length && j>=0 && j<board[i].length && board[i][j]!= '#' && wordIndex<word.length() 
+            && word.charAt(wordIndex)==board[i][j])
     	{
     		if(wordIndex==word.length()-1)
     		{
     			return true;
     		}
-    		board[i][j] = '0';
+            char c = board[i][j];
+    		board[i][j] = '#';
     		boolean top = exist(board,word,wordIndex+1,i-1,j);
     		boolean bottom = exist(board,word,wordIndex+1,i+1,j);
     		boolean left = exist(board,word,wordIndex+1,i,j-1);
@@ -44,6 +73,10 @@ class Solution {
     		{
     			return true;
     		}
+            else
+            {
+                board[i][j] = c;
+            }
     	}
     	return false;
     }
