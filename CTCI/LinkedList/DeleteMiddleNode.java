aa33a8fcbba9549670205af1a4c83e6b121c5834
Pritesh.Bhere <pbhere11@gmail.com>
2018-06-16 15:29:15 -0400

    2.3 delete middle node

---
+++ CTCI/LinkedList/DeleteMiddleNode.java
@@ -0,0 +1,90 @@
+/*
+2.3 Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+that node.
+EXAMPLE
+Input: the node c from the linked list a - >b- >c - >d - >e- >f
+Result: nothing is returned, but the new linked list looks like a - >b- >d - >e- >f
+Hints: #72
+*/
+
+class Wrapper {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static void prettyPrintLinkedList(ListNode node) {
+      while (node != null && node.next != null) {
+          System.out.print(node.val + "->");
+          node = node.next;
+      }
+    
+      if (node != null) {
+        System.out.println(node.val);
+      } else {
+        System.out.println("Empty LinkedList");
+      }
+    }
+}
+
+public class MainClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        ListNode head = null;
+        while ((line = in.readLine()) != null) {
+            ListNode node = Wrapper.stringToListNode(line);
+            head = node;
+            Wrapper.prettyPrintLinkedList(node);
+        }
+       	ListNode node = head;
+       	while(node.val!=3)
+       	{
+       		node = node.next;
+       	}
+       	MainClass mn = new MainClass();
+       	mn.deleteNode(node);
+       	Wrapper.prettyPrintLinkedList(head);
+    }
+
+    public void deleteNode(ListNode node)
+    {
+    	node.val = node.next.val;
+		node.next = node.next.next;
+    }
+}
