86cf491f19efd1e5605f52c2d886feeab273cca2
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-06-20 13:35:00 -0400

    updating the structure

---
+++ C#/CTCI/Arrays and Strings/1.1 Is Unique/IsUnique.cs
@@ -0,0 +1,16 @@
+/*
+1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+*/
+
+public class MainClass {
+    public static void Main() {
+        
+    }
+
+    public void isUnique(string str)
+    {
+    	
+    }
+
+}
