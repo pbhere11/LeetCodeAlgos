22b3bf1958a3dad93f708830ddd0e3dd1abb5013
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-06-21 10:53:17 -0400

    Best Time to Buy and Sell Stock II

--- CSharp/Easy/Array/BestTimeToBuyAndSellStockII.cs
+++ CSharp/Easy/Array/BestTimeToBuyAndSellStockII.cs
@@ -20,6 +20,23 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 public class Solution {
     public int MaxProfit(int[] prices) {
-        
+    	if(prices.Length==0)
+    	{
+    		return 0;
+    	}
+        int currentSmallest = prices[0];
+        int currentHighest = prices[0];
+        int maxProfit =0;
+        for(int i=1;i<prices.Length;i++)
+        {
+        	if(prices[i]<currentHighest)
+        	{
+        		maxProfit = maxProfit + (currentHighest-currentSmallest);
+        		currentSmallest = prices[i];
+        	}
+        	currentHighest = prices[i];
+        }
+        maxProfit = maxProfit + (currentHighest-currentSmallest);
+        return maxProfit;
     }
 }
