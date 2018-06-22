2eaded51bfaed4a2ff2fda6a6d49b79c9c7667cf
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-06-22 15:09:21 -0400

    valid sudoku

--- CSharp/Easy/Array/ValidSudoku.cs
+++ CSharp/Easy/Array/ValidSudoku.cs
@@ -32,3 +32,55 @@ Output: false
 Explanation: Same as Example 1, except with the 5 in the top left corner being 
     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 */
+    public class Solution {
+    public bool IsValidSudoku(char[,] board) {
+        bool[,] row = new bool[9,9];
+        bool[,] column = new bool[9,9];
+        bool[,] square = new bool[9,9];
+
+        
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                //check rows
+                if(board[i,j]!='.')
+                {
+                   if(row[i,(int)board[i,j]-'1'])
+                   {
+                      return false;
+                   }
+                   else
+                   {
+                      row[i,(int)board[i,j]-'1'] = true;
+                   }
+                }
+                //check columns
+                if(board[i,j]!='.')
+                {
+                   if(column[j,(int)board[i,j]-'1'])
+                   {
+                      return false;
+                   }
+                   else
+                   {
+                      column[j,(int)board[i,j]-'1'] = true;
+                   }
+                }
+                //check squares
+                if(board[i,j]!='.')
+                {
+                   if(square[i/3*3 + j/3,(int)board[i,j]-'1'])
+                   {
+                      return false;
+                   }
+                   else
+                   {
+                      square[i/3*3 + j/3,(int)board[i,j]-'1'] = true;
+                   }
+                }
+            }
+        }
+        return true;
+    }
+}
