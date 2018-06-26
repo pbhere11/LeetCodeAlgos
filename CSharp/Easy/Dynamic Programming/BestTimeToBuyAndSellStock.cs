5beefcd3a24864bd50abd2d6ff014ca1399a8c03
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-06-26 12:24:13 -0400

    best time to buy and sell stock

--- CSharp/Easy/Dynamic Programming/BestTimeToBuyAndSellStock.cs
+++ CSharp/Easy/Dynamic Programming/BestTimeToBuyAndSellStock.cs
@@ -13,6 +13,27 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 public class Solution {
     public int MaxProfit(int[] prices) {
-        
+        if(prices.Length==0|| prices.Length==1)
+        {
+        	return 0;
+        }
+        int min = prices[0];
+        int max = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1;i<prices.Length;i++)
+        {
+        	if(prices[i]<min)
+        	{
+        		min = prices[i];
+        		max = prices[i];
+        	}
+        	else if(prices[i]>max)
+        	{
+        		max = prices[i];
+        	}
+        	maxProfit = Math.Max(maxProfit,max-min);
+        }
+        return maxProfit;
     }
 }
