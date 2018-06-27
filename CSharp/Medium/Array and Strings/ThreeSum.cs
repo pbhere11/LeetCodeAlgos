4b6195486e8e6456802a9d91b6f764c0de9e7603
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-06-27 16:35:15 -0400

    adding folder for medu=ium

---
+++ CSharp/Medium/Array and Strings/ThreeSum.cs
@@ -0,0 +1,21 @@
+/*
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+[-1, 0, 1, 2, -1, -4]
+-4, -1, -1, 0, 1, 2
+
+*/
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        
+        IList<IList<int>> returnList = new List<>
+    }
+}
