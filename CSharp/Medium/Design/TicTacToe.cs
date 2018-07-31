79ed464238fa76506830c6313d6ffb70eafac669
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-07-30 22:56:58 -0400

    TicTacToe

---
+++ CSharp/Medium/Design/TicTacToe.cs
@@ -0,0 +1,91 @@
+/*
+Example:
+Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+TicTacToe toe = new TicTacToe(3);
+
+toe.move(0, 0, 1); -> Returns 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+toe.move(0, 2, 2); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+toe.move(2, 2, 1); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+toe.move(1, 1, 2); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+toe.move(2, 0, 1); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+toe.move(1, 0, 2); -> Returns 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+*/
+public class TicTacToe {
+
+    int[] rows;
+    int[] cols;
+    int dg1;
+    int dg2;
+    int n;
+    /** Initialize your data structure here. */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+        this.n = n;
+    }
+    
+    /** Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins. */
+    public int Move(int row, int col, int player) {
+        int val = player==1?1:-1;
+        rows[row]+=val;
+        cols[col]+=val;
+        if(row==col)
+        {
+            dg1+=val;
+        } 
+        if(col == n-row-1)
+        {
+            dg2+=val;
+        }
+        if(Math.Abs(rows[row])==n ||
+           Math.Abs(cols[col])==n ||
+           Math.Abs(dg1)==n ||
+           Math.Abs(dg2)==n)
+        {
+            return player;
+        }
+        return 0;
+    }
+}
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.Move(row,col,player);
+ */
