0456a79e711753d585bb2ed851ea1da55451fb3a
Pritesh Bhere <pritesh.bhere@reisystems.com>
2018-08-01 22:41:33 -0400

    Tree To Linked List

---
+++ CSharp/PracticeSet2/Linked List/FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	TreeNode prevNode=null;
+    public void Flatten(TreeNode root) {
+        if(root!=null)
+        {
+        	Flatten(root.right);
+        	Flatten(root.left);
+        	root.left = null;
+        	root.right = prevNode;
+        	prevNode = root;
+        }
+    }
+}
